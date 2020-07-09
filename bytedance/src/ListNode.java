fc9e0f3b7358c3aa296ecdead71405a859677875
liyang201 <liyang201@meituan.com>
2020-07-09 08:53:12 +0800

    题解

---
+++ bytedance/src/ListNode.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+/**
+ * @Author liyang
+ * @Date 2020/7/8 6:23 下午
+ * @Description 单向链表节点类
+ **/
+public class ListNode {
+    public int value;
+    public ListNode next;
+
+    public ListNode(int value){
+        this.value = value;
+    }
+}
