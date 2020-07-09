0485bd554a0b99cbee9a846f4944660916ecea2d
liyang <liyang201@meituan.com>
2020-07-09 09:04:19 +0800

    modify

--- zuoshen/src/binaryTree/GetMaxLength.java
+++ zuoshen/src/binaryTree/GetMaxLength.java
@@ -1,4 +1,13 @@
 package binaryTree;
 
 public class GetMaxLength {
+    public int getMaxLength(Node head, int sum){
+        if (head == null)
+            return sum == 0 ? 0 : Integer.MIN_VALUE;
+
+        int left = Math.max(getMaxLength(head.left, sum - head.value) + 1, getMaxLength(head.left, sum));
+        int right = Math.max(getMaxLength(head.right, sum - head.value) + 1, getMaxLength(head.right, sum));
+
+        return Math.max(left, right);
+    }
 }
