1fff90047496d501989fb582d143f64e74049cbd
liyang201 <liyang201@meituan.com>
2020-07-13 20:51:59 +0800

    算法题

--- bytedance/src/Solution.java
+++ bytedance/src/Solution.java
@@ -248,4 +248,24 @@ public class Solution {
             right--;
         }
     }
+
+    // 33
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        int middle = left + (right - left) / 2;
+        while (left <= right){
+            if (nums[middle] == target){
+                return middle;
+            }else if (nums[middle] >= nums[left] && target >= nums[left] && target < nums[middle]){
+                right = middle - 1;
+            }else{
+                left = middle + 1;
+            }
+            middle = left + (right - left) / 2;
+        }
+        return -1;
+    }
 }
