ed4c8bca7cb75f64536b00046d06e5d23513e09d
liyang <liyang201@meituan.com>
2020-07-14 21:10:02 +0800

    modify

--- bytedance/src/thread/Semaph.java
+++ bytedance/src/thread/Semaph.java
@@ -5,5 +5,11 @@ import java.util.concurrent.Semaphore;
 public class Semaph {
     Semaphore semaphore = new Semaphore(1);
 
+    public static void main(String[] args) {
+        // 获得了锁
+        // 执行await，进入等待队列，释放锁
+        // 其它线程single或singleall，当前线程尝试获取锁
+        // 获取到了继续执行，没获取到继续再等待队列等待
+    }
 
 }
